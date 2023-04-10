7307a50a06cf544d71f7153b0915569a9365983b
tahsinraza95 <tahsinr693@gmail.com>
2023-04-10 22:23:27 +0530

    [TahsinRaza] Adding Part Time Employee Wage

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -11,19 +11,27 @@ namespace EmployeeWageCompute
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Employee Wage Program");
-
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            //UC3
+            if (empCheck == IS_PART_TIME)
             {
+                Console.WriteLine("Employee is Part Time Present");
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Full Time Present");
                 empHrs = 8;
             }
             else
             {
+                Console.WriteLine("Employee is Absent");
                 empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;

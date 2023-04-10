f65efd2721382cc6e162f975992767f31b2892e2
tahsinraza95 <tahsinr693@gmail.com>
2023-04-10 22:35:37 +0530

    [TahsinRaza] Adding SwitchCase

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -10,32 +10,30 @@ namespace EmployeeWageCompute
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To The Employee Wage Program");
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            //UC3
-            if (empCheck == IS_PART_TIME)
+            int empCheck = random.Next(0, 3);
+            //UC4 Switch Case
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Part Time Present");
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is Full Time Present");
-                empHrs = 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("EmpWage : " + empWage);
+            Console.WriteLine("Employee wage is:" + empWage);
             Console.ReadLine();
         }
     }

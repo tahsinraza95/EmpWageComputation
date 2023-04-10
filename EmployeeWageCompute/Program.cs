565abd7a2b6bbe0545556f564952c29b50ee4ad4
tahsinraza95 <tahsinr693@gmail.com>
2023-04-10 22:59:33 +0530

    [TahsinRaza] Adding Calculate Wage

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -8,19 +8,21 @@ namespace EmployeeWageCompute
 {
     public class Program
     {
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
+            const int NUM_OF_WORKING_DAYS = 20;
+            const int MAX_HRS_IN_MONTH = 10;
             static void Main(string[] args)
             {
 
-                const int IS_PART_TIME = 1;
-                const int IS_FULL_TIME = 2;
-                const int EMP_RATE_PER_HOUR = 20;
-                const int NUM_OF_WORKING_DAYS = 20;
+
                 int empHrs = 0;
-                int empWage = 0;
-                int totalEmpWage = 0;
+                int totalworkingDays = 10;
+                int totalEmpHrs = 0;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
-                for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+                while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalworkingDays < NUM_OF_WORKING_DAYS)
                 {
                     //UC4 Switch Case
                     switch (empCheck)
@@ -36,12 +38,13 @@ namespace EmployeeWageCompute
                             break;
 
                     }
-                    empWage = empHrs * EMP_RATE_PER_HOUR;
-                    totalEmpWage += empWage;
-                    Console.WriteLine("Employee wage is:" + empWage);
+                    totalEmpHrs += empHrs;
+                    Console.WriteLine("Day#:" + totalworkingDays + "Emp Hrs :" + empHrs);
                 }
+                int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
                 Console.WriteLine("Total Emp Wage :" + totalEmpWage);
                 Console.ReadLine();
             }
         }
     }
+

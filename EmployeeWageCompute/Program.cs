1e8f4989dd64600cbbad743ad5f9c493fe41c265
tahsinraza95 <tahsinr693@gmail.com>
2023-04-10 21:59:09 +0530

    [TahsinRaza] Adding Employee Wage

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -10,7 +10,19 @@ namespace EmployeeWageCompute
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
+            Console.WriteLine("Welcome To The Employee Wage Program");
+
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
             Console.ReadLine();
         }
     }
